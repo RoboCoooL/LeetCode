8265a361726a817948f778227a54fbf33f87f7ea
Sergei <cooolman.ne@gmail.com>
2024-07-08 15:06:47 +0200

    Initial commit

---
+++ LeetCode/Problems/Problem26.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problems;
+/// <summary>
+/// 26. Remove Duplicates from Sorted Array
+/// https://leetcode.com/problems/remove-duplicates-from-sorted-array
+/// </summary>
+public class Problem26
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var numsDict = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            numsDict.TryAdd(num, 0);    
+        }
+
+        var k = numsDict.Keys.Count;
+        
+        Array.Copy(numsDict.Keys.ToArray(), 0, nums, 0, k);
+
+        return k;
+    }
+
+    
+    [Test]
+    [TestCase(new[] {1,1,2}, new[]{1,2,0}, ExpectedResult = 2)]
+    public int Test(int[] nums, int[] numsExpected)
+    {
+        var ret = RemoveDuplicates(nums);
+        Assert.That(nums[..ret], Is.EqualTo(numsExpected[..ret]));
+        return ret;
+    }
+}
---
+++ LeetCode/Problems/Problem88.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems;
+
+
+
+/// <summary>
+/// 88. Merge Sorted Array
+/// https://leetcode.com/problems/merge-sorted-array/ 
+/// </summary>
+public class Problem88
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+       Array.Copy(nums2,0,nums1,m,n);
+       Array.Sort(nums1);
+    }
+    
+    [Test]
+    [TestCase(new[]{1,0},1, new[]{2},1, new[]{1,2})]
+    [TestCase(new[]{1,2,3,0,0,0},3, new[]{2,5,6},3, new[]{1,2,2,3,5,6})]
+    public void Test(int[] nums1, int m, int[] nums2, int n, int[] res)
+    {
+        Merge(nums1, m, nums2, n);
+        Assert.That(nums1, Is.EqualTo(res));
+    }
+    
+}
