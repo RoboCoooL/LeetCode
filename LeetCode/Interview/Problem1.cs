8de4d2c5f6ae2d8ba0ffa7957aaec9164a8a47d8
Sergei <cooolman.ne@gmail.com>
2024-07-09 14:29:17 +0200

    Added problem 20

--- LeetCode/Interview/Problem1.cs
+++ LeetCode/Interview/Problem1.cs
@@ -25,6 +25,6 @@ public class Problem1
     [InlineData(new[] {2, 7, 11, 15}, 9, new[] {0, 1})]
     [InlineData(new[] {3, 2, 4}, 6, new[] {1, 2})]
     [InlineData(new[] {3, 3}, 6, new[] {0, 1})]
-    public void CanTwoSum(int[] nums, int target, int[] expectedNums)
-        => TwoSum(nums, target).Should().BeEquivalentTo(expectedNums);
+    public void CanTwoSum(int[] nums, int target, int[] expectedNums) =>
+        TwoSum(nums, target).Should().BeEquivalentTo(expectedNums);
 }
---
+++ LeetCode/Interview/Problem20.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Interview;
+/// <summary>
+///     20. Valid Parentheses
+///     https://leetcode.com/problems/valid-parentheses
+/// </summary>
+public class Problem20
+{
+    public bool IsValid(string s)
+    {
+        Dictionary<char, char> bracketsMap = new() {{'{', '}'}, {'[', ']'}, {'(', ')'}};
+        Stack<char> openBrackets = new();
+        foreach ( char c in s )
+        {
+            if ( bracketsMap.ContainsKey(c) )
+            {
+                openBrackets.Push(c);
+            }
+            else
+            {
+                if ( !openBrackets.TryPop(out char bracket) )
+                {
+                    return false;
+                }
+
+                if ( c != bracketsMap[bracket] )
+                {
+                    return false;
+                }
+            }
+        }
+
+        return openBrackets.Count <= 0;
+    }
+
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("(]", false)]
+    public void CanIsValid(string s, bool expected) =>
+        IsValid(s).Should().Be(expected);
+}
