9e2eb6b3eaa9513af9b2f17e8d458e7faa4d7cb6
Sergei <cooolman.ne@gmail.com>
2024-07-10 14:36:18 +0200

    Added 125

---
+++ LeetCode/Interview/Problem125.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.Interview;
+/// <summary>
+///     125. Valid Palindrome
+///     https://leetcode.com/problems/valid-palindrome
+/// </summary>
+public class Problem125
+{
+    public bool IsPalindrome(string s)
+    {
+        string str = Regex.Replace(s.ToLowerInvariant(), @"[^a-zA-Z0-9]", "");
+        int endIndex = str.Length - 1;
+        int startIndex = 0;
+        foreach ( char c in str )
+        {
+            if ( startIndex == endIndex )
+            {
+                break;
+            }
+
+            if ( c != str[endIndex] )
+            {
+                return false;
+            }
+
+            startIndex++;
+            endIndex--;
+        }
+
+        return true;
+    }
+
+    [Theory]
+    [InlineData("A man, a plan, a canal: Panama", true)]
+    [InlineData("race a car", false)]
+    [InlineData(" ", true)]
+    [InlineData("abcba", true)]
+    [InlineData("abccba", true)]
+    public void CanIsPalindrome(string s, bool expected) => IsPalindrome(s).Should().Be(expected);
+}
