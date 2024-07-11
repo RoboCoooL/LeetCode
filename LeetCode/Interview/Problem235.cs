c4a271e864203e4f5c2c86239fc3acf979c8c546
Sergei <cooolman.ne@gmail.com>
2024-07-11 17:09:33 +0200

    wip problem 235

---
+++ LeetCode/Interview/Problem235.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace LeetCode.Interview;
+/// <summary>
+///     235. Lowest Common Ancestor of a Binary Search Tree
+///     https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+/// </summary>
+public class Problem235
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) => root;
+
+    [Theory]
+    [ClassData(typeof(LowestCommonAncestorTestData))]
+    public void CanLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q, TreeNode expected) =>
+        LowestCommonAncestor(root, p, q).Should().BeEquivalentTo(expected);
+}
+
+public class LowestCommonAncestorTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        TreeNode root = new([6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]);
+        TreeNode p = new([2, 0, 4, null, null, 3, 5]);
+        TreeNode q = new([8, 7, 9]);
+        TreeNode expected = new([6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]);
+
+        yield return [root, p, q, expected];
+
+        root = new TreeNode([6, 2, 8, 0, 4, 7, 9, null, null, 3, 5]);
+        p = new TreeNode([2, 0, 4, null, null, 3, 5]);
+        q = new TreeNode([4, 3, 5]);
+        expected = new TreeNode([2, 0, 4, null, null, 3, 5]);
+
+        yield return [root, p, q, expected];
+
+        root = new TreeNode([2, 1]);
+        p = new TreeNode([2, 1]);
+        q = new TreeNode([1]);
+        expected = new TreeNode([2, 1]);
+
+        yield return [root, p, q, expected];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ LeetCode/Interview/TreeNode.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.Interview;
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode
+{
+    public TreeNode left;
+    public TreeNode right;
+    public int val;
+
+    public TreeNode(int x) => val = x;
+
+    // public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    // {
+    //     this.val = val;
+    //     this.left = left;
+    //     this.right = right;
+    // }
+
+    public TreeNode(List<int?> nodes)
+    {
+        if ( nodes == null || nodes.Count == 0 )
+        {
+            return;
+        }
+
+        val = (int)nodes[0]!;
+        Queue<TreeNode> q = new();
+        q.Enqueue(this);
+
+        int i = 1;
+        while ( i < nodes.Count )
+        {
+            TreeNode curr = q.Dequeue();
+            int? nextVal = nodes[i++];
+            if ( nextVal != null )
+            {
+                curr.left = new TreeNode((int)nextVal);
+                q.Enqueue(curr.left);
+            }
+
+            if ( i >= nodes.Count )
+            {
+                return;
+            }
+
+            nextVal = nodes[i++];
+            if ( nextVal != null )
+            {
+                curr.right = new TreeNode((int)nextVal);
+                q.Enqueue(curr.right);
+            }
+        }
+    }
+}
