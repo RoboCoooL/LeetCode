de92ee0c5d22d5419fd6060fc49230865facd5f5
Sergei <cooolman.ne@gmail.com>
2024-07-12 16:48:11 +0200

    wip 232

---
+++ LeetCode/Interview/Problem232.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Interview;
+/// <summary>
+///     232. Implement Queue using Stacks
+///     https://leetcode.com/problems/implement-queue-using-stacks
+/// </summary>
+public class Problem232
+{
+}
+
+public class MyQueue
+{
+    public void Push(int x)
+    {
+    }
+
+    public int Pop()
+    {
+    }
+
+    public int Peek()
+    {
+    }
+
+    public bool Empty()
+    {
+    }
+}
