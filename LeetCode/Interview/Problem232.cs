f1d8eb495398c22071ac96711e3eea2ab9c1255e
Sergei <cooolman.ne@gmail.com>
2024-07-16 12:30:45 +0200

    Add unit tests and implement MyQueue class methods

--- LeetCode/Interview/Problem232.cs
+++ LeetCode/Interview/Problem232.cs
@@ -5,23 +5,49 @@
 /// </summary>
 public class Problem232
 {
+    [Fact]
+    public void MyQueueTest()
+    {
+        MyQueue myQueue = new();
+        Queue<int> queue = new();
+        myQueue.Push(1); // queue is: [1]
+        queue.Enqueue(1);
+        myQueue.S1.Should().BeEquivalentTo(queue);
+        myQueue.Push(2); // queue is: [1, 2] (leftmost is front of the queue)
+        queue.Enqueue(2);
+        myQueue.S1.Should().BeEquivalentTo(queue);
+        int peek = queue.Peek();
+        myQueue.Peek().Should().Be(peek); // return 1
+        int dequeue = queue.Dequeue();
+        myQueue.Pop().Should().Be(dequeue); // return 1, queue is [2]
+        myQueue.Empty().Should().BeFalse(); // return false
+    }
 }
 
 public class MyQueue
 {
+    private readonly Stack<int> _s2 = new();
+    public Stack<int> S1 { get; } = new();
+
+
     public void Push(int x)
     {
+        while ( S1.Count > 0 )
+        {
+            _s2.Push(S1.Pop());
+        }
+
+        S1.Push(x);
+
+        while ( _s2.Count > 0 )
+        {
+            S1.Push(_s2.Pop());
+        }
     }
 
-    public int Pop()
-    {
-    }
+    public int Pop() => S1.Pop();
 
-    public int Peek()
-    {
-    }
+    public int Peek() => S1.Peek();
 
-    public bool Empty()
-    {
-    }
+    public bool Empty() => S1.Count == 0;
 }
